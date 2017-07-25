ba91bf00a047ae0ef33fad282e7d412b26cc2c6c
Mattias Holmlund <mattias.git@holmlund.se>
2017-07-25 09:41:24 +0200

    Only detect changes

--- monitor-chromecast.ts
+++ monitor-chromecast.ts
@@ -3,8 +3,23 @@ import * as mdns from 'mdns'
 import { EventEmitter } from 'events'
 
 let requestId = 1
+export type PowerState = 'on' | 'off'
+export type PlayState = 'play' | 'pause'
+export type Media = {
+    artist: string
+    title: string
+}
 
 export class ChromecastMonitor extends EventEmitter {
+    public powerState: PowerState = 'off'
+    public playState: PlayState = 'pause'
+    public currentMedia: Media = {
+        artist: 'none',
+        title: 'none',
+    }
+
+    private found = false
+
     constructor(private friendlyName: string) {
         super()
         let browser = mdns.createBrowser(mdns.tcp('googlecast'))
@@ -14,7 +29,12 @@ export class ChromecastMonitor extends EventEmitter {
         let requestId = 1
 
         browser.on('serviceUp', service => {
-            if (service.txtRecord.fn === this.friendlyName) {
+            if (service.txtRecord.fn === this.friendlyName && !this.found) {
+                // If the same device is reachable via several
+                // network interfaces, it will be detected
+                // once per interface.
+                this.found = true
+                console.log('device_found', service)
                 this.createClient(service.addresses[0])
                 browser.stop()
             }
@@ -23,6 +43,7 @@ export class ChromecastMonitor extends EventEmitter {
     }
 
     createClient(address) {
+        console.log('createClient')
         let client = new Client()
         client.connect(address, () => {
             const connection = client.createChannel(
@@ -71,14 +92,45 @@ export class ChromecastMonitor extends EventEmitter {
                     type: 'GET_STATUS',
                     requestId: requestId++,
                 })
-                media.on('message', songInfo => parseData(songInfo))
+                media.on('message', songInfo => this.parseData(songInfo))
             })
         })
     }
-}
 
-function parseData(songInfo) {
-    console.log('songInfo', JSON.stringify(songInfo))
+    setPlayState(playerState) {
+        let playState: PlayState = 'play'
+
+        if (playerState === 'PAUSED') {
+            playState = 'pause'
+        }
+
+        if (this.playState !== playState) {
+            this.playState = playState
+            console.log('playState', playState)
+        }
+    }
+
+    setMedia(media: Media) {
+        if (
+            media.artist !== this.currentMedia.artist ||
+            media.title !== this.currentMedia.title
+        ) {
+            this.currentMedia = media
+            console.log(this.currentMedia)
+        }
+    }
+
+    parseData(songInfo) {
+        //        console.log(songInfo.status)
+        let status = songInfo.status[0]
+        this.setPlayState(status.playerState)
+        if (status.media) {
+            this.setMedia({
+                artist: status.media.metadata.artist,
+                title: status.media.metadata.title,
+            })
+        }
+    }
 }
 
 let cm = new ChromecastMonitor('Garage')
